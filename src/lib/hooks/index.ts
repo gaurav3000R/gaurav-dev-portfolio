716191b7c1cef45844f08acab5ed919f442c130e
GAURAV RATHVA <gauravr@digiflux.io>
2026-01-16 09:34:17 +0530

    feat: Add barrel export for custom hooks in `src/lib/hooks`.

---
+++ src/lib/hooks/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Hooks - Barrel Export
+ * 
+ * Centralized exports for all custom hooks
+ */
+
+export { useMouseInteraction } from './useMouseInteraction';
+export { useScrollIntegration } from './useScrollIntegration';
+export { useScrollAnimation } from './useScrollAnimation';
+export { useSoundEffects } from './useSoundEffects';
+export { useSpaceshipAudio } from './useSpaceshipAudio';
